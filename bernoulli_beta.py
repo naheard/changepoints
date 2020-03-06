b54880b038a628ac6d870e73eeabc97d9590107c
N <naheard@hotmail.com>
2020-03-06 11:04:23 +0000

    adding bernoulli_beta class

---
+++ bernoulli_beta.py
@@ -0,0 +1,8 @@
+from multinomial_dirichlet import MultinomialDirichlet
+from data import Data
+import numpy as np
+from scipy.special import gammaln
+
+class BernoulliBeta(MultinomialDirichlet):
+    def __init__(self,data=None,p=1,alpha_beta=[1,1]):
+        MultinomialDirichlet.__init__(self,data=data,k=np.repeat(2,p),alpha=np.tile(np.array(alpha_beta),[1,p]))
